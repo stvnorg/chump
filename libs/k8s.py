e6c92d51dc0b4d8082fd6b4a8464ead5b4a8395c
Steven Stevanus <stvn.org@gmail.com>
2020-11-22 23:33:53 -0600

    update the libraries

--- libs/k8s.py
+++ libs/k8s.py
@@ -2,18 +2,19 @@ import json
 from kubernetes import client, config
 
 class K8s:
-    def __init__(self, namespace, deployment_name):
+    def __init__(self, namespace, deployment_name, container_name):
         config.load_kube_config()
         self.v1 = client.AppsV1Api()
         self.namespace = namespace
         self.deployment_name = deployment_name
+        self.container_name = container_name
 
-    def check_image_update(self, container_name, image_version):
+    def check_image_update(self, image_version):
         self.ret = self.v1.read_namespaced_deployment(self.deployment_name, self.namespace)
         self.deployment_metadata = json.loads(self.ret.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'])
 
         for deployment in self.deployment_metadata['spec']['template']['spec']['containers']:
-            if deployment['name'] == container_name and deployment['image'] == image_version:
+            if deployment['name'] == self.container_name and deployment['image'] == image_version:
                 return False
 
         return True
--- libs/ops.py
+++ libs/ops.py
@@ -3,9 +3,14 @@ import yaml
 from subprocess import run
 
 class Ops:
-    def __init__(self, logging, k8s):
+    def __init__(self, logging, k8s, git_url, branch, deploy_path, container_name, image_version_file):
         self.logging = logging
         self.k8s = k8s
+        self.git_url = git_url
+        self.branch = branch
+        self.deploy_path = deploy_path
+        self.container_name = container_name
+        self.image_version_file = image_version_file
 
     def get_image_version(self, container_name, image_version_file):
         with open(image_version_file, 'r') as f:
@@ -20,34 +25,34 @@ class Ops:
                     except:
                         pass
 
-    def clone_and_deploy(self, git_url, branch, deploy_path, container_name, image_version_file):
+    def clone_and_deploy(self):
         try:
             root_dir = os.getcwd()
-            args = ["git", "clone", git_url]
+            args = ["git", "clone", self.git_url]
             run(args, check=True)
 
-            working_dir = git_url.split('/')[::-1][0]
+            working_dir = self.git_url.split('/')[::-1][0]
             working_dir = working_dir.split('.')[0]
 
             os.chdir(working_dir)
 
-            args = ["git", "checkout", branch]
+            args = ["git", "checkout", self.branch]
             self.logging.info(run(args, check=True))
 
             self.logging.info(self.k8s)
 
-            self.image_version = self.get_image_version(container_name, image_version_file)
+            self.image_version = self.get_image_version(self.container_name, self.image_version_file)
             self.logging.info(self.image_version)
 
-            if self.k8s.check_image_update(container_name, self.image_version):
-                args = "kustomize build {} | kubectl apply -f -".format(deploy_path)
+            if self.k8s.check_image_update(self.image_version):
+                args = "kustomize build {} | kubectl apply -f -".format(self.deploy_path)
                 self.logging.info(args)
                 self.logging.info(os.system(args))
             else:
                 self.logging.info("No new update!")
 
             os.chdir(root_dir)
-            args = ["rm", "-rf", os.path.join(os.getcwd(), working_dir)]
+            args = ["rm", "-rf", os.path.join(root_dir, working_dir)]
             self.logging.info(run(args, check=True))
 
         except Exception as exception_msg:
